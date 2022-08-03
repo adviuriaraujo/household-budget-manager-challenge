c9f2aec5cec4af3f9bff3e3241735fcda23ab4cc
adviuriaraujo <85589853+adviuriaraujo@users.noreply.github.com>
2022-08-03 19:30:55 -0300

    feat: adds UPDATE and DELETE routes for expenses and adds Postman Collection

--- src/controllers/DespesaController.js
+++ src/controllers/DespesaController.js
@@ -32,6 +32,44 @@ class DespesaController {
             next(erro);
         }
     }
+
+    static async atualizaDespesa(req, res, next) {
+        try {
+            const { id } = req.params;
+            const dadosAtualizados = req.body;
+            const despesaCadastrada = await despesaServices.pegaUmRegistro(id);
+            if (dadosAtualizados.data && dadosAtualizados.descricao) {
+                await despesaServices.verificaDespesasDuplicadas(dadosAtualizados, id);
+            }
+            if (dadosAtualizados.data && !dadosAtualizados.descricao) {
+                await despesaServices.verificaDespesasDuplicadas({
+                    descricao: despesaCadastrada.descricao,
+                    data: dadosAtualizados.data,
+                }, id);
+            }
+            if (!dadosAtualizados.data && dadosAtualizados.descricao) {
+                await despesaServices.verificaDespesasDuplicadas({
+                    descricao: dadosAtualizados.descricao,
+                    data: despesaCadastrada.data,
+                }, id);
+            }
+            await despesaServices.atualizaRegistro(dadosAtualizados, id);
+            const despesaAtualizada = await despesaServices.pegaUmRegistro(id);
+            return res.status(200).json(despesaAtualizada);
+        } catch (erro) {
+            next(erro);
+        }
+    }
+
+    static async removeDespesa(req, res, next) {
+        try {
+            const { id } = req.params;
+            await despesaServices.removeRegistro(id);
+            return res.status(200).json({ mensagem: `Despesa de id ${id} removida com sucesso!` })
+        } catch (erro) {
+            next(erro);
+        }
+    }
 }
 
 module.exports = { DespesaController };
--- src/routes/despesaRoutes.js
+++ src/routes/despesaRoutes.js
@@ -7,7 +7,7 @@ router
     .get('/despesas', DespesaController.pegaTodasDespesas)
     .get('/despesas/:id', DespesaController.pegaUmaDespesa)
     .post('/despesas', DespesaController.cadastraDespesa)
-    // .put('/despesas/:id', DespesaController.atualizaDespesa)
-    // .delete('/despesas/:id', DespesaController.removeDespesa)
+    .put('/despesas/:id', DespesaController.atualizaDespesa)
+    .delete('/despesas/:id', DespesaController.removeDespesa)
 
 module.exports = router;
